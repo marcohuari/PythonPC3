012a4dde7a2af8fe45a5e1d82d58b43fb3e2a0fa
marcohuari <60158378+marcohuari@users.noreply.github.com>
2024-05-25 02:50:41 +0000

    PC3

--- Problema10/main.py
+++ Problema10/main.py
@@ -1,6 +1,6 @@
 
-from Figuras import circulo
-from Figuras import cuadrilatero
+from circulo import *
+from cuadrilatero import *
 
 MENU = """
 1. Calcular el área de un circulo
@@ -9,16 +9,40 @@ MENU = """
 4. Salir.
 """
 
+def main():
+    print('Bienvenido')
+    
+    while(True):
+        opc = int(input(MENU))
+        if opc == 1:
+            r = int(input('Digite el radio del círculo: '))
+            c1 = Circulo(r)
+            area = c1.calcular_area_circulo()
+            print(area)
+            pass
+        elif opc == 2:
+            b = int(input('Digite la base del rectángulo: '))
+            h = int(input('Digite la altura del rectángulo: '))
+            r1 = Rectangulo(b,h)
+            area = r1.calcular_area()
+            print(area)
+            pass
+        elif opc == 3:
+            lado = int(input('Digite el lado del cuadrado: '))
+            c1 = Cuadrado(lado)
+            area = c1.calcular_area()
+            print(area)
+            pass
+        elif opc == 4:
+            print('Saliendo del programa.')
+            break
+        else:
+            break
+        pass
+    pass
 
-
-
-opc = int(input(MENU))
-
-if opc == 1:
-    r = int('Digite el radio del círculo: ')
-    c1 = Circulo(r)
-    area = c1.calcular_area_radio()
-    print(area)
+if __name__ == '__main__':
+    main()
 
     
     
